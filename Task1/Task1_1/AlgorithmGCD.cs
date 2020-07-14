3ad538010809edaeb2c4030c2c73442fe817aa78
ivanhalauko <van_delta@mail.ru>
2020-07-14 16:21:02 +0300

    3. Created Euclidean algorithm calculates (GCD) of two,three,four natural numbers numOne, numTwo, numThree and numFour. Created Test Cases.

--- Task1/Task1_1/AlgorithmGCD.cs
+++ Task1/Task1_1/AlgorithmGCD.cs
@@ -4,8 +4,8 @@ using System.Diagnostics;
 
 namespace Task1_1ClassLibrary
 {
-    public class AlgorithmGCD
-    {
+	public class AlgorithmGCD
+	{
 		/// <summary>
 		/// The Euclidean algorithm calculates (GCD) of two natural numbers numOne and numTwo.
 		/// </summary>
@@ -24,10 +24,48 @@ namespace Task1_1ClassLibrary
 				}
 				else
 				{
-					numTwo = numOne - numTwo;
+					numTwo = numTwo- numOne;
 				}
-			}		
-			return Math.Abs(numOne);
+			}
+			return numOne;
+		}
+
+		/// <summary>
+		/// The method calculates (GCD) of three natural numbers numOne, numTwo, numThree.
+		/// </summary>
+		/// <param name="numOne">number one</param>
+		/// <param name="numTwo">number two</param>
+		/// <param name="numThree">number three</param>
+		/// <returns>GCD of three numbers</returns>
+		public int EuclideanAlgorithm(int numOne, int numTwo, int numThree)
+		{
+			numOne = Math.Abs(numOne);
+			numTwo = Math.Abs(numTwo);
+			numThree = Math.Abs(numThree);
+			int tempNumb = EuclideanAlgorithm(numOne, numTwo);
+			int gcdThreeNumb = EuclideanAlgorithm(tempNumb, numThree);
+			return gcdThreeNumb;
+
+		}
+
+		/// <summary>
+		/// The method calculates (GCD) of four natural numbers numOne, numTwo, numThree, numFour.
+		/// </summary>
+		/// <param name="numOne"></param>
+		/// <param name="numTwo"></param>
+		/// <param name="numThree"></param>
+		/// <param name="numFour"></param>
+		/// <returns>GCD of four numbers</returns>
+		public int EuclideanAlgorithm(int numOne, int numTwo, int numThree, int numFour)
+		{
+			numOne = Math.Abs(numOne);
+			numTwo = Math.Abs(numTwo);
+			numThree = Math.Abs(numThree);
+			numFour = Math.Abs(numFour);
+			int gcdTwoNumb = EuclideanAlgorithm(numOne, numTwo);
+			int gcdThreeNumb = EuclideanAlgorithm(gcdTwoNumb, numThree);
+			int gcdFourNumb = EuclideanAlgorithm(gcdThreeNumb, numFour);
+			return gcdFourNumb;
 		}
 	}
 }
--- Task1/Task1_1ClassLibraryTests/AlgorithmGCDTests.cs
+++ Task1/Task1_1ClassLibraryTests/AlgorithmGCDTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1_1ClassLibrary
+namespace Task1_1ClassLibrary.Tests
 {
 	[TestFixture()]
 	public class AlgorithmGCDTests
@@ -16,17 +16,55 @@ namespace Task1_1ClassLibrary
 		/// </summary>
 		/// <param name="numOne"></param>
 		/// <param name="numTwo"></param>
-		/// <returns>GCD result</returns>
-		[TestCase(200,100,ExpectedResult =100,TestName = "GivenEuclideanAlgorithm_WhenForTwoNumb_200_100_ThenIs100")]
+		/// <returns>GCD 2 numbers result</returns>
+		[TestCase(200, 100, ExpectedResult = 100, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumb_200_100_ThenIs100")]
 		[TestCase(16, 8, ExpectedResult = 8, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumb_16_8_ThenIs8")]
-		[TestCase(-200,-100, ExpectedResult = 100, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumb_-200_-100_ThenIs100")]
-		[TestCase(-16, -8, ExpectedResult = 8, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumb_-16_-8_ThenIs8")]
+		[TestCase(-200, -100, ExpectedResult = 100, TestName = "GivenEuclideanAlgorithm_WhenForTwoNegativeNumb_-200_-100_ThenIs100")]
+		[TestCase(-16, -8, ExpectedResult = 8, TestName = "GivenEuclideanAlgorithm_WhenForTwoNegativeNumb_-16_-8_ThenIs8")]
 		[TestCase(-16, 8, ExpectedResult = 8, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumbNegativeAndPositive_-16_8_ThenIs8")]
 		[TestCase(-4, 8, ExpectedResult = 4, TestName = "GivenEuclideanAlgorithm_WhenForTwoNumbNegativeAndPositive_-4_8_ThenIs4")]
 		public int EuclideanAlgorithm_ForTwoNumb_Test(int numOne, int numTwo)
 		{
 			var calculator = new AlgorithmGCD();
 			return calculator.EuclideanAlgorithm(numOne, numTwo);
+		}
+
+		/// <summary>
+		/// Test check up method's EuclideanAlgorithm result of three numbers.
+		/// </summary>
+		/// <param name="numOne"></param>
+		/// <param name="numTwo"></param>
+		/// <param name="numThree"></param>
+		/// <returns>GCD 3 numbers result</returns>
+		[TestCase(200, 100,10, ExpectedResult = 10, TestName = "GivenEuclideanAlgorithm_WhenForThreeNumb_200_100_10_ThenIs10")]
+		[TestCase(16, 8, 4, ExpectedResult = 4, TestName = "GivenEuclideanAlgorithm_WhenForThreeNumb_16_8_4_ThenIs4")]
+		[TestCase(-200, -100, -10, ExpectedResult = 10, TestName = "GivenEuclideanAlgorithm_WhenForThreeNegativeNumb_200_100_10_ThenIs10")]
+		[TestCase(-16, -8, -4, ExpectedResult = 4, TestName = "GivenEuclideanAlgorithm_WhenForThreeNegativeNumb_-16_-8_-4_ThenIs4")]
+		[TestCase(-16, -8, 4, ExpectedResult = 4, TestName = "GivenEuclideanAlgorithm_WhenForThreeNegativeAndPositiveNumb_-16_-8_4_ThenIs4")]
+		[TestCase(-4, -8, 2, ExpectedResult =2, TestName = "GivenEuclideanAlgorithm_WhenForThreeNegativeAndPositiveNumb_-4_-8_2_ThenIs2")]
+		public int EuclideanAlgorithmTest(int numOne, int numTwo, int numThree)
+		{
+			var calculator = new AlgorithmGCD();
+			return calculator.EuclideanAlgorithm(numOne,numTwo,numThree);
+		}
+
+		/// <summary>
+		/// Test check up method's EuclideanAlgorithm result of three numbers.
+		/// </summary>
+		/// <param name="numOne"></param>
+		/// <param name="numTwo"></param>
+		/// <param name="numThree"></param>
+		/// <returns>GCD 3 numbers result</returns>
+		[TestCase(200, 100, 10,5, ExpectedResult = 5, TestName = "GivenEuclideanAlgorithm_WhenForFourNumb_200_100_10_5_ThenIs5")]
+		[TestCase(16, 8, 4,2, ExpectedResult = 2, TestName = "GivenEuclideanAlgorithm_WhenForFourNumb_16_8_4_2_ThenIs2")]
+		[TestCase(-200, -100, -10,-5, ExpectedResult = 5, TestName = "GivenEuclideanAlgorithm_WhenForFourNegativeNumb_-200_-100_-10_-5_ThenIs5")]
+		[TestCase(-16, -8, -4,-2, ExpectedResult = 2, TestName = "GivenEuclideanAlgorithm_WhenForFourNegativeNumb_-16_-8_-4_-2_ThenIs2")]
+		[TestCase(-16, -8, 4,2, ExpectedResult = 2, TestName = "GivenEuclideanAlgorithm_WhenForFourNegativeAndPositiveNumb_-16_-8_4_2_ThenIs2")]
+		[TestCase(-4, -8, 2,10, ExpectedResult = 2, TestName = "GivenEuclideanAlgorithm_WhenForFourNegativeAndPositiveNumb_-4_-8_2_10_ThenIs2")]
+		public int EuclideanAlgorithmTest(int numOne, int numTwo, int numThree,int numFour)
+		{
+			var calculator = new AlgorithmGCD();
+			return calculator.EuclideanAlgorithm(numOne, numTwo, numThree,numFour);
 		}
 	}
 }
