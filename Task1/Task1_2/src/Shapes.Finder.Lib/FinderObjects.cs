f50c03c6e558da26681cf0d775f33d940b7055cd
ivanhalauko <van_delta@mail.ru>
2020-07-17 16:45:13 +0300

    1.4. Created class FinderObjects with Method FindElements() and test method FindElementsTest()

---
+++ Task1/Task1_2/src/Shapes.Finder.Lib/FinderObjects.cs
@@ -0,0 +1,27 @@
+﻿using src.Shapes.Model.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shapes.Finder.Lib
+{
+	public class FinderObjects
+	{
+		private BaseShape _baseShape;
+		private BaseShape[] _baseShapes;
+
+		public FinderObjects(BaseShape[] baseShapes, BaseShape baseShape)
+		{
+			_baseShape = baseShape;
+			_baseShapes = baseShapes;
+		}
+
+		public BaseShape[] FindElements()
+		{
+			IEnumerable<BaseShape> elements = _baseShapes.ToList().FindAll(x => x.Equals(_baseShape));
+			BaseShape[] elementsBase = elements.ToList().ToArray();
+
+			return elementsBase;
+		}
+	}
+}
---
+++ Task1/Task1_2/src/Shapes.Finder.LibTests/FinderObjectsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Shapes.Finder.Lib;
+using Shapes.Reader.File.Creater;
+using src.Shapes.Model.Lib;
+using System;
+
+namespace Shapes.Finder.Lib.Tests
+{
+	[TestFixture()]
+	public class FinderObjectsTests
+	{
+		string path = @"textFile\FileWithShapes.txt";
+		[TestCase()]
+		public void FindElementsTest()
+		{
+			//Arrange
+			BaseShape[] expectedObject = new BaseShape[] { new Circle(35) };
+			CreaterArrayFigures createArrayFigures = new CreaterArrayFigures(path);
+			//Act
+			BaseShape[] baseShapes = createArrayFigures.GetBaseShapes();
+
+			FinderObjects finderObjects = new FinderObjects(baseShapes, new Circle(35));
+			var actual = finderObjects.FindElements();
+			//Asssert
+			Assert.AreEqual(expectedObject, actual);
+		}
+	}
+}
