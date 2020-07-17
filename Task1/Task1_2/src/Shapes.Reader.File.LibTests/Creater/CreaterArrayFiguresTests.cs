062925ed4bf255c7749a571041373de0740b591a
ivanhalauko <van_delta@mail.ru>
2020-07-17 16:22:08 +0300

    1.3. Created class CreaterArrayFigures and test for GetBaseShapes method.

---
+++ Task1/Task1_2/src/Shapes.Reader.File.LibTests/Creater/CreaterArrayFiguresTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using src.Shapes.Model.Lib;
+using Shapes.Reader.File.Creater;
+
+namespace Shapes.Reader.File.LibTests.Creater
+{
+	[TestFixture()]
+	public class CreaterArrayFiguresTests
+	{
+		string path = @"textFile\FileWithShapes.txt";
+		[TestCase()]
+		public void GetBaseShapesTest()
+		{
+			//Arrange
+			BaseShape[] expectedBaseShapes = new BaseShape[] { new Circle(35), new Rectangle(64, 64.7) };
+			CreaterArrayFigures createrArrayFigures = new CreaterArrayFigures(path);
+			//Act
+			var actual = createrArrayFigures.GetBaseShapes();
+
+			//Asssert
+			Assert.AreEqual(expectedBaseShapes, actual);
+		}
+	}
+}
---
+++ Task1/Task1_2/src/Shapes.Reader.File/Creater/CreaterArrayFigures.cs
@@ -0,0 +1,52 @@
+﻿using Shapes.Reader.File.Utilities;
+using src.Shapes.Model.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Reader.File.Creater
+{
+	public class CreaterArrayFigures
+	{
+		public FileReader _fileReader;
+		public CreaterArrayFigures(string path)
+		{
+			_fileReader = new FileReader(path);
+		}
+
+		public BaseShape[] GetBaseShapes()
+		{
+			var rows = _fileReader.GetRows();
+			var rowsLength = rows.Length;
+			BaseShape[] baseShapes = new BaseShape[rowsLength];
+
+			for (int i = 0; i < rowsLength; i++)
+			{
+				var rowElements = rows[i].Split(' ');
+				string name = rowElements[0];
+				switch (name)
+				{
+					case "circle":
+						{
+							baseShapes[i] = new Circle(Convert.ToDouble(rowElements[1]));
+							break;
+						}
+					case "rectangle":
+						{
+							baseShapes[i] = new Rectangle(Convert.ToDouble(rowElements[1]), Convert.ToDouble(rowElements[2]));
+							break;
+						}
+					case "square":
+						{
+							baseShapes[i] = new Square(Convert.ToDouble(rowElements[1]));
+							break;
+						}
+					default: break;
+				}
+			}
+			return baseShapes;
+		}
+	}
+}
