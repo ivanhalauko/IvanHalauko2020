21b787cb33db688237bba621d775ed8b289cb099
ivanhalauko <van_delta@mail.ru>
2020-07-17 15:27:21 +0300

    1.2. Created File Reader and test method GetRowsTest, changed location FileWithShapes.txt

---
+++ Task1/Task1_2/src/Shapes.Reader.File.LibTests/Utilities/FileReaderTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+//using Shapes.Reader.File.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Reader.File.Utilities.Tests
+{
+	[TestFixture()]
+	public class FileReaderTests
+	{
+		string path = @"textFile\FileWithShapes.txt";
+		[TestCase()]
+		public void GetRowsTest()
+		{
+			//Arrange
+			string[] expected = new string[] { "circle 35", "rectangle 64 64,7" };
+			FileReader fileReader = new FileReader(path);
+			//Act
+
+			var actual = fileReader.GetRows();
+
+			//Assert
+			Assert.AreEqual(expected: expected, actual: actual);
+		}
+	}
+}
---
+++ Task1/Task1_2/src/Shapes.Reader.File/Utilities/FileReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+
+namespace Shapes.Reader.File.Utilities
+{
+	public class FileReader
+	{
+		public string Path { get; private set; }
+
+		public FileReader(string path)
+		{
+			Path = path;
+		}
+
+		public string[] GetRows()
+		{
+			var rows = GetString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+			return rows;
+		}
+
+		private string GetString()
+		{
+			string allFile = string.Empty;
+			try
+			{
+				using (StreamReader reader = new StreamReader(Path, System.Text.Encoding.Default))
+				{
+					string line;
+					int i = 0;
+					while ((line = reader.ReadLine()) != null)
+					{
+						allFile += line + "\n";
+					}
+				}
+			}
+			catch (Exception)
+			{
+				throw new Exception();
+			}
+			return allFile;
+		}
+	}
+}
