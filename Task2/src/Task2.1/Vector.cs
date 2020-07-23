b3c87a25130266eec354d90dc5854a3e6cedecbc
ivanhalauko <van_delta@mail.ru>
2020-07-23 12:39:34 +0300

    2.1.1 Created new branche, folder, project and test's folder for Task2.1

---
+++ Task2/src/Task2.1/Vector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task201
+{
+    public class Vector
+    {
+		public void Method()
+		{ }
+
+		public void Method2()
+		{ }
+	}
+}
---
+++ Task2/tests/Task2.1Tests/VectorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Task201;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task201.Tests
+{
+	[TestFixture()]
+	public class VectorTests
+	{
+		[Test()]
+		public void MethodTest()
+		{
+			Assert.Fail();
+		}
+
+		[Test()]
+		public void Method2Test()
+		{
+
+		}
+	}
+}
