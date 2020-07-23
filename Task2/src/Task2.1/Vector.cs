d3629173a6dff7a9b581856d2ea9436d7aced76c
ivanhalauko <van_delta@mail.ru>
2020-07-23 13:36:53 +0300

    2.1.2. Created field, property and Vector class constructor.

--- Task2/src/Task2.1/Vector.cs
+++ Task2/src/Task2.1/Vector.cs
@@ -8,10 +8,38 @@ namespace Task201
 {
     public class Vector
     {
-		public void Method()
-		{ }
+		private double x;
+		private double y;
+		private double z;
 
-		public void Method2()
-		{ }
+
+		/// <summary>
+		/// vector properties
+		/// </summary>
+		public double X
+		{
+			get { return this.x; }
+		}
+		public double Y
+		{
+			get { return this.y; }
+		}
+		public double Z
+		{
+			get { return this.z; }
+		}
+
+		/// <summary>
+		/// The constructor initializes the tree-dimension vector
+		/// </summary>
+		/// <param name="x">The first vector dimension</param>
+		/// <param name="y">The second vector dimension</param>
+		/// <param name="z">The third vector dimension</param>
+		public Vector(double x, double y, double z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
 	}
 }
