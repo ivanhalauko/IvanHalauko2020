12bcd88490b7c8ad0820c266b650d7f9d8ac736b
ivanhalauko <van_delta@mail.ru>
2020-07-24 13:21:23 +0300

    2.3.1 Created new branche, folder, project and test's folder for Task2.3 and BaseProductClass.

---
+++ Task2/src/Task2.3/Product.Model.Lib/BaseProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Model.Lib
+{
+    public abstract class BaseProduct
+    {
+		public void Method()
+		{ }
+    }
+}
