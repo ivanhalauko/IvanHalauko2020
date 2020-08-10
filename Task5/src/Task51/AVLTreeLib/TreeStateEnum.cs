bf1356406be72295dc44cb466eed06547851308d
ivanhalauko <van_delta@mail.ru>
2020-08-10 20:46:15 +0300

    Task5.1. Created AVLTreeNode.

---
+++ Task5/src/Task51/AVLTreeLib/TreeStateEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task51.AVLTreeLib
+{
+    enum TreeStateEnum
+    {
+        Balanced, //Дерево сбалансировано
+        LeftHeavy, //В левом поддереве больше элементов
+        RightHeavy, //В правом поддереве больше элементов
+    }
+}
