d8372380d71ecf4088636bb0336df279fd27996c
ivanhalauko <van_delta@mail.ru>
2020-09-07 12:07:33 +0300

    Task6. 23. Created SingletonAccessToDbo.

---
+++ Task6/src/DAO.DataAccesLayer/Singleton/SingletonAccessToDbo.cs
@@ -0,0 +1,30 @@
+﻿using DAO.DataAccesLayer.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.DataAccesLayer.Singleton
+{
+    public class SingletonAccessToDbo
+    {
+        private static SingletonAccessToDbo _instance = null;
+
+        public AbstractFactory Factory { get; set; } = null;
+
+        private SingletonAccessToDbo(AbstractFactory factory)
+        {
+            AbstractFactory Factory = factory;
+        }
+
+        public static SingletonAccessToDbo GetInstance(AbstractFactory factory)
+        {
+            if (_instance==null)
+            {
+                _instance = new SingletonAccessToDbo(factory);  
+            }
+            return _instance;
+        }
+    }
+}
