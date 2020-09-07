ce7081c6bf1a04d3a6a3611f8e97160138bf199d
ivanhalauko <van_delta@mail.ru>
2020-09-07 11:54:02 +0300

    Task 6. 22. Created abstracFactory and ADOFactory into DAO.AccesLayer.

---
+++ Task6/src/DAO.DataAccesLayer/Factory/ADOFactory.cs
@@ -0,0 +1,64 @@
+﻿using ModelsInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.DataAccesLayer.Factory
+{
+    public class ADOFactory : AbstractFactory
+    {
+        protected internal ADO<Exam> Exam { get; set; }
+
+        protected internal ADO<ExamForGroup> ExamForGroup { get; set; }
+
+        protected internal ADO<ExamStudResults> ExamStudResults { get; set; }
+
+        protected internal ADO<ExamTerms> ExamTerms { get; set; }
+
+        protected internal ADO<Groups> Groups { get; set; }
+
+        protected internal ADO<Students> Students { get; set; }
+
+        public ADOFactory(string dbConnectionString)
+        {
+            Exam = new ADO<Exam>(dbConnectionString);
+            ExamForGroup = new ADO<ExamForGroup>(dbConnectionString);
+            ExamStudResults = new ADO<ExamStudResults>(dbConnectionString);
+            ExamTerms = new ADO<ExamTerms>(dbConnectionString);
+            Groups = new ADO<Groups>(dbConnectionString);
+            Students = new ADO<Students>(dbConnectionString);
+        }
+
+        public override ICRUD<Exam> CreateExam()
+        {
+            return Exam;
+        }
+
+        public override ICRUD<ExamForGroup> CreateExamForGroup()
+        {
+            return ExamForGroup;
+        }
+
+        public override ICRUD<ExamStudResults> CreateExamStudResults()
+        {
+            return ExamStudResults;
+        }
+
+        public override ICRUD<ExamTerms> CreateExamTerms()
+        {
+            return ExamTerms;
+        }
+
+        public override ICRUD<Groups> CreateGroups()
+        {
+            return Groups;
+        }
+
+        public override ICRUD<Students> CreateStudents()
+        {
+            return Students;
+        }
+    }
+}
---
+++ Task6/src/DAO.DataAccesLayer/Factory/AbstractFactory.cs
@@ -0,0 +1,26 @@
+﻿using ModelsInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.DataAccesLayer.Factory
+{
+    public abstract class AbstractFactory
+    {
+        public abstract ICRUD<Students> CreateStudents();
+
+        public abstract ICRUD<Groups> CreateGroups();
+
+        public abstract ICRUD<ExamTerms> CreateExamTerms();
+
+        public abstract ICRUD<ExamStudResults> CreateExamStudResults();
+
+        public abstract ICRUD<ExamForGroup> CreateExamForGroup();
+
+        public abstract ICRUD<Exam> CreateExam();
+
+
+    }
+}
