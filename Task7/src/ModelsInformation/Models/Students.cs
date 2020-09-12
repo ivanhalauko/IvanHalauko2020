1717c3d0f75301993c77c3bfda140a75d0573184
ivanhalauko <van_delta@mail.ru>
2020-09-12 22:09:27 +0300

    Task7. 2.Added tosolution ModelsInfirmation from task6.

---
+++ Task7/src/ModelsInformation/Models/Students.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelsInformation
+{
+    [Table("Students")]
+    /// <summary>
+    /// Student's class.
+    /// </summary>
+    public class Students : ISubstance
+    {
+        [Key]
+        /// <summary>
+        /// Implement interface ISubstance. 
+        /// </summary>
+        public int Id { get ; set; }
+        /// <summary>
+        /// Student's name property.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Student's surname property.
+        /// </summary>
+        public string Surname { get; set; }
+        /// <summary>
+        /// Student's patronymic property.
+        /// </summary>
+        public string Patronymic { get; set; }
+        /// <summary>
+        /// Student's birth date property.
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Student's groupe ID property.
+        /// </summary>
+        public int IDGroupe { get; set; }
+
+        
+
+    }
+}
