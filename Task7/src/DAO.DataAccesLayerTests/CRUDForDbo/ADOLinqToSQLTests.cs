c068c5a2c862e4e6c9a96a98963b9e52514f50d7
IvanHalauko <van_delta@mail.ru>
2020-09-28 13:14:36 +0300

    Task7. 13. Added test create/read/update/delete element to ExaminersTable in database.

--- Task7/src/DAO.DataAccesLayerTests/CRUDForDbo/ADOLinqToSQLTests.cs
+++ Task7/src/DAO.DataAccesLayerTests/CRUDForDbo/ADOLinqToSQLTests.cs
@@ -1,11 +1,11 @@
-﻿using NUnit.Framework;
-using System.Linq;
+﻿using System.Linq;
+using NUnit.Framework;
 using ModelsInformation.Models;
 
 namespace DAO.DataAccesLayer.Tests
 {
     /// <summary>
-    /// Test class for testing CRUD methods.
+    /// Test class for testing CRUD (LINQ to SQL) methods.
     /// </summary>
     [TestFixture()]
     public class ADOLinqToSQLTests
@@ -15,16 +15,24 @@ namespace DAO.DataAccesLayer.Tests
         /// </summary>
         private ICRUD<Specialties> specialties;
 
+        private ICRUD<Examiners> examiners;
+
+        /// <summary>
+        /// Private field connection string. 
+        /// </summary>
         private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SQLServer.Database_task7;Integrated Security = True; Connect Timeout=30 ; Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=True";
+
         /// <summary>
         /// Test cases for Add, Read and Delete CRUD-methods.
         /// </summary>
-        /// <param name="actualName">Name parameter.</param>
-        [TestCase("NewModernSpecialties")]
-        public void ReadAllAndAddAndDelete_ThenOutIsActualName(string actualName)
+        /// <param name="actualName">Parameter's name.</param>
+        [TestCase("Sailor", "Intresting conditions of work")]
+        [TestCase("AncientSpecialities", "Bad conditions of work")]
+        [TestCase("NewModernSpecialities", "Good conditions of work")]
+        public void ReadAllAndAddAndDelete_ThenOutIsActualNameOfSpecialtiesTable(string actualName, string infoOfSpecialty)
         {
             //Arrage
-            Specialties actual = new Specialties() { Id = 5, NameOfSpecialty = actualName, InfoOfSpecialty = "asd" };
+            Specialties actual = new Specialties() { Id = 5, NameOfSpecialty = actualName, InfoOfSpecialty = infoOfSpecialty };
             //Act
             specialties = new ADOLinqToSQL<Specialties>(connectionString);
             specialties.CreateElement(actual);
@@ -35,13 +43,16 @@ namespace DAO.DataAccesLayer.Tests
             //Assert
             Assert.AreEqual(lastAddedName, actualName);
         }
+
         /// <summary>
         /// Test cases for Update CRUD-method.
         /// </summary>
         /// <param name="name">Name of object's field.</param>
-        /// <param name="actualUpdateName">Actual update name</param>
-        [TestCase("NameSpecialteTest3", "VaseFour")]
-        public void GivenUpdate_WhenAdded_ThenOutUpdateThisSpecialties(string name, string actualUpdateName)
+        /// <param name="actualUpdateName">Actual update name.</param>
+        [TestCase("NameSpecialityeTest1", "NewNameSpecialityTest1")]
+        [TestCase("NameSpecialityeTest2", "NewNameSpecialityTest2")]
+        [TestCase("NameSpecialityeTest3", "NewNameSpecialityTest3")]
+        public void GivenUpdate_WhenAdded_ThenOutUpdateName_ThisSpecialtiesTable(string name, string actualUpdateName)
         {
             //Arrage
             Specialties actual = new Specialties() { NameOfSpecialty = name };
@@ -54,6 +65,57 @@ namespace DAO.DataAccesLayer.Tests
             specialties.DeleteElement(lastAddedId); //clear last addded element
             //Assert
             Assert.AreEqual(expectedUpdetedName.NameOfSpecialty, actualUpdateName);
+        }
+
+        /// <summary>
+        /// Test cases for Add, Read and Delete CRUD-methods.
+        /// </summary>
+        /// <param name="id">Id of Examiners.</param>
+        /// <param name="name">Name of Examiners.</param>
+        /// <param name="surname">Surname of Examiners.</param>
+        /// <param name="patronymic">Patronymic of Examiners.</param>
+        [TestCase(10, "Ivan", "Ivanovich", "Ivanov")]
+        [TestCase(10, "Grigoriy", "Grigorievich", "Grigoriev")]
+        [TestCase(10,"Petr", "Petrovich","Petrov")]
+        public void ReadAllAndAddAndDelete_ThenOutIsActualNameOfExaminersTable(int id,string actualName, string surname, string patronymic)
+        {
+            //Arrage
+            Examiners actual = new Examiners() { Id = id, Name = actualName, Surname = surname, Patronymic= patronymic };
+            //Act
+            examiners = new ADOLinqToSQL<Examiners>(connectionString);
+            examiners.CreateElement(actual);
+
+            var lastAddedName = examiners.ReadAllElementFromDatabase().ToList().Last().Name;
+            var lastAddedId = examiners.ReadAllElementFromDatabase().ToList().Last().Id;
+            examiners.DeleteElement(lastAddedId);
+            //Assert
+            Assert.AreEqual(lastAddedName, actualName);
+        }
+
+        /// <summary>
+        /// Test cases for Update CRUD-method.
+        /// </summary>
+        /// <param name="name">Name of object's field.</param>
+        /// <param name="actualUpdateName">Actual update name.</param>
+        //[TestCase("NameSpecialityeTest1", "NewNameSpecialityTest1")]
+        //[TestCase("NameSpecialityeTest2", "NewNameSpecialityTest2")]
+        [TestCase("Ivan", "Ivanovich", "Ivanov", "Grigoriy")]
+        [TestCase("Grigoriy", "Grigorievich", "Grigoriev", "Grigoriy")]
+        [TestCase("Petr" , "Petrovich", "Petrov", "Grigoriy")]
+        public void GivenUpdate_WhenAdded_ThenOutUpdateName_ThisExaminersTable(string name, string surname, string patronymic, string actualUpdateName)
+        {
+            //Arrage
+            Examiners actual = new Examiners() { Name = name, Surname = surname, Patronymic = patronymic };
+            //Act
+            examiners = new ADOLinqToSQL<Examiners>(connectionString);
+            examiners.CreateElement(actual);
+            
+            var lastAddedId = examiners.ReadAllElementFromDatabase().ToList().Last().Id;
+            examiners.UpdateDatabase(new Examiners() { Id = lastAddedId, Name = actualUpdateName, Surname = surname, Patronymic = patronymic });
+            var expectedUpdetedName = examiners.ReadElementFromDatabase(lastAddedId);
+            examiners.DeleteElement(lastAddedId); //clear last addded element
+            //Assert
+            Assert.AreEqual(expectedUpdetedName.Name, actualUpdateName);
         }
     }
 }
